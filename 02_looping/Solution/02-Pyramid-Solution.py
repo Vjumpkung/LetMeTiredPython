24a45e7ed98fe6b26dc9066f6991b737fca4cf47
vjumpkung <vjumpkung@gmail.com>
2022-11-16 16:07:07 +0700

    edit solution

--- 02_looping/Solution/02-Pyramid-Solution.py
+++ 02_looping/Solution/02-Pyramid-Solution.py
@@ -2,6 +2,10 @@ t = input("symbol : ")
 n = int(input("size : "))
 c = 0
 for i in range(n):
-    print(f"{' '*(n-i-1)}{t*(2*i+1)}")
+    for j in range(n-i-1):
+        print(" ", end="")
+    for j in range(2*i+1):
+        print("*", end="")
+    print()
     c += 2*i+1
 print(f"Use {t} {c} items")
