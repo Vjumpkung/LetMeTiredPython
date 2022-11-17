97258ebea90d776e3798b0bb84a996e7b1e67f2e
Chanrich Pisitjing <vjumpkung@gmail.com>
2022-11-17 13:02:22 +0700

    coorection 02-Pyramid-Solution.py

--- 02_looping/Solution/02-Pyramid-Solution.py
+++ 02_looping/Solution/02-Pyramid-Solution.py
@@ -5,7 +5,7 @@ for i in range(n):
     for j in range(n-i-1):
         print(" ", end="")
     for j in range(2*i+1):
-        print("*", end="")
+        print(t, end="")
     print()
     c += 2*i+1
 print(f"Use {t} {c} items")
